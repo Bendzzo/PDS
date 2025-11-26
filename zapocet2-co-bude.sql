8ba9572572cc435a5f050ae640a3409e6a1aeee8
Bc. Benjamín Vyhnal <benjamin.vyhnal@gmail.com>
2025-11-26 15:18:23 +0100

    Create zapocet2-co-bude.sql
    Add SQL comments outlining course topics and restrictions.

---
+++ zapocet2-co-bude.sql
@@ -0,0 +1,18 @@
+-- transakcie, insert, savepoint, rollback
+
+--prakticky json - tie si treba prejst z uloh
+
+-- prakticke indexy, otzky typu vytvorte vhodny index nad danym..., STROMY, B+ tree, rozne typy pristupov, Sposoby spajania tabuliek
+
+-- pokrocila statistika a analytika -- case, sum, ...
+
+-- DBLINK -- vytvorenie dblinku, alebo bude vyvoreny a pomocou neho pristupte k inej DB
+
+-- Teoria na BLOB, CLOB, BFILE
+
+-- ABCD - 10. prednaska - aky je hardware - treba pozriet obidve prednasky, merge join, hash join, transakcie...
+
+-- budu aj teoreticke doplnovacky typu co je to transakcia...
+
+-- ******** Kurzory ani generovanie prikazov NEBUDU!! ******
+-- kolekcie NEBUDU, XML tiez, outer join ani exists ani objekty
